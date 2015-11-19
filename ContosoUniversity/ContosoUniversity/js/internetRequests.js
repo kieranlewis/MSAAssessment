def06cc4c234bc9a75e9a45743539780407e3c1d
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-19 14:40:27 +1300

    created basic index page and made the azure website

---
+++ ContosoUniversity/ContosoUniversity/js/internetRequests.js
@@ -0,0 +1,29 @@
+﻿var StudentModule = (function () {
+    return {
+        //function to get all students from database
+        getStudents: function (callback) {
+
+            var xhttp = new XMLHttpRequest();
+            
+            //This gets triggered when the state of the xhttp object changes
+            xhttp.onreadystatechange = function () {
+                // 4 - repsonse is ready, 200 success code
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    loadedStudents();
+                }
+            }
+            
+            // Build up our request and send it - true for async
+            xhttp.open("GET", "http://test7549.azurewebsites.net/api/Students", true);
+            xhttp.setRequestHeader("Content-type", "application/json");
+            xhttp.send(null);
+            
+            // Parse and send the studentlist data back to index.js
+            function loadedStudents() {
+                var studentsList = JSON.parse(xhttp.responseText);
+                callback(studentsList);
+                return studentsList;
+            }
+        }
+    }
+});
