22f3b1a4075cbb25a0dc2687720db549515a9f04
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-20 20:10:52 +1300

    created models and context for backend part of web site

---
+++ MSAAssessment/MSAAssessment/DAL/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MSAAssessment.Models;
+
+namespace MSAAssessment.DAL
+{
+    public class SchoolContext
+    {
+        public SchoolContext()
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
