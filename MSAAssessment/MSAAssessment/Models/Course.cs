9eb5109d675212e4815d81ac1afe2a220b4772c6
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-20 19:37:37 +1300

    created basic web pages

---
+++ MSAAssessment/MSAAssessment/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MSAAssessment.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string courseName { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+    }
+}
