36d10bdfb668931a25ba93ebf6aea9cf31f1d0bb
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-22 11:07:16 +1300

    created models

---
+++ MSAAssessment/MSAAssessment/Models/StudentWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSAAssessment.Models
+{
+    public class StudentWork
+    {
+        public int StudentWorkID { get; set; }
+        public int StudentID { get; set; }
+        public string linkName { get; set; }
+    }
+}
