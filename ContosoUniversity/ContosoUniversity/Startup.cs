aad9b9dafb3c33d8262e9d999b387522e8a62919
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-23 19:28:59 +1300

    added chat room

---
+++ ContosoUniversity/ContosoUniversity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ContosoUniversity.Startup))]
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
