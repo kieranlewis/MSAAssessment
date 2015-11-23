e7a212b49333b22675fe2f40e5d5dfcb1de16af5
Kieran Lewis <kieranlewis1334@gmail.com>
2015-11-23 17:20:47 +1300

    added controllers

---
+++ ContosoUniversity/ContosoUniversity/Models/StudentWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class StudentWork
+    {
+        public int StudentWorkID { get; set; }
+        public int StudentID { get; set; }
+        public string LinkName { get; set; }
+    }
+}
